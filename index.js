2aba428a7aa806acd6f2140af84b3728d08b9b62
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 03:12:14 +0000

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Value){
+  
+ if ( Value <= 400) {
+   return "This one is on me!";
+ } else if ( Value > 2000 && Value < 2500) {
+   
+   return "I will gladly take your thirty bucks.";
+   
+ } else if ( Value > 2500) {
+   return "No can do.";
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  
+  return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+} 
+function switchOnCharmFromTip(tip){
+    switch (tip){
+    case "generous": return "That is great"; break;
+    case "not as generous": return "uhhh"; break;
+    
+     default: return "Bye.";
+
+ }
+}
 }
